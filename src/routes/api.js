15ce477eaeafd050c11a478b2e285fa9998daaa9
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-08-01 18:06:02 +0700

    handle refresh account

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const { createUser, handleLogin, getUser } = require('../controllers/userController')
+const { createUser, handleLogin, getUser, getAccount } = require('../controllers/userController')
 const auth = require('../middleware/auth')
+const delay = require('../middleware/delay')
 
 const routerAPI = express.Router()
 
@@ -14,5 +15,6 @@ routerAPI.post("/register", createUser)
 routerAPI.post("/login", handleLogin)
 
 routerAPI.get("/user", getUser)
+routerAPI.get("/account", delay, getAccount)
 
 module.exports = routerAPI //export default
